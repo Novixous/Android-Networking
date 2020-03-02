a315152553740493074833bfd205c4e162bec9b7
Cao Thu <thanhthu0321@gmail.com>
2020-03-02 23:17:30 +0700

    Viet them giao dien xml va code activity theo slide. Chưa run.

--- NetworkingClient/app/src/main/java/khanglna/com/networkingclient/MainActivity.java
+++ NetworkingClient/app/src/main/java/khanglna/com/networkingclient/MainActivity.java
@@ -2,13 +2,21 @@ package khanglna.com.networkingclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends Activity {
     private ImageView img;
     private TextView txt;
     private EditText edtWord;
@@ -27,9 +35,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img = findViewById(R.id.i)
+        img = (ImageView) findViewById(R.id.img);
+        txt = (TextView) findViewById(R.id.txtText);
+        edtWord = (EditText) findViewById(R.id.edtWord);
+        txtResult = (TextView) findViewById(R.id.txtService);
+        txtJson = (TextView) findViewById(R.id.txtJson);
+        edtMsg = (EditText) findViewById(R.id.editMessage);
+        txtMsgReceived = (TextView) findViewById(R.id.txtMsgReceived);
     }
 
     public void clickToSend(View view) {
+        MyClientTask client = new MyClientTask(IP_ADDRESS,Integer.parseInt(edtMsg.getText().toString()));
+        client.execute();
+    }
+
+    private class MyClientTask extends AsyncTask<Void, Void, Void> {
+        private String dstAddress;
+        private int dstPort;
+        private String response = "";
+
+        public MyClientTask(String dstAddress, int dstPort) {
+            this.dstAddress = dstAddress;
+            this.dstPort = dstPort;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Socket socket = null;
+            try {
+                socket = new Socket(dstAddress, dstPort);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+                byte[] buffer = new byte[1024];
+                int byteRead;
+                InputStream inputStream = socket.getInputStream();
+                while ((byteRead=inputStream.read(buffer))!=-1){
+                    byteArrayOutputStream.write(buffer,0,byteRead);
+                    response+=byteArrayOutputStream.toString("UTF-8");
+                }
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                response="UnknowHostException: "+e.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                response="IOException: "+e.toString();
+            }finally {
+                if (socket!=null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
